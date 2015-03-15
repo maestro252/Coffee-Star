c52c5e250e90525312b2e97ff8324ca31746b855
Jean Pierre Gomez <jgomezb@eafit.edu.co>
2015-03-14 20:18:29 -0500

    Added folder structure for Visual Studio project

---
+++ p2/CoffeeStar/CoffeeStar/CoffeeStar.cpp
@@ -0,0 +1,17 @@
+// CoffeeStar.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	cin >> name;
+	cout << "Your name is: " << name << endl;
+	return 0;
+}
+
