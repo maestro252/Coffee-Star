3ed86c40f522ee0ae218e3b5b6fbbcd2bcf9f936
Jonathan Eidelman Manevich <jonyxmaster@gmail.com>
2015-03-16 15:36:20 -0500

    Reading from file to vectors, working perfectly

--- p2/CoffeeStar/CoffeeStar/CoffeeStar.cpp
+++ p2/CoffeeStar/CoffeeStar/CoffeeStar.cpp
@@ -4,16 +4,51 @@
 //#include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-	string name;
-	cin >> name;
-	cout << "Your name is: " << name << endl;
-	for(int i=0; i < 5; ++i){
-		cout << "Hola" << endl;
+	ifstream in("in.txt");
+	string line;
+	stringstream ss;
+	vector<string> names;
+	vector<string> ids;
+	vector<long> salaries;
+	while(getline(in, line))
+	{
+		//cout << line << endl;
+			for(int i = 0; i < line.length() - 1; ++i){
+				if(line[i] == ';') {
+					ids.push_back(line.substr(0, i));
+					line = line.substr(i+1, line.length() - 1);
+					break;
+				}
+			}
+
+
+			for(int j = 0; j < line.length() - 1; ++j)
+			{
+				if(line[j] == ';'){
+					names.push_back(line.substr(0, j));
+					line = line.substr(j + 1, line.length() - 1);
+					break;
+				}
+			}
+			string salaryS = line.substr(0, line.length()-1);
+			stringstream s(salaryS);
+			long salary;
+			s >> salary;
+			salaries.push_back(salary);
 	}
+
+	for(int i = 0; i < names.size(); ++i){
+		cout << "Cedula: " << ids[i] << " Nombre: " << names[i] << " salario: ";
+		cout << salaries[i] << endl;
+	}
+
 	return 0;
 }
