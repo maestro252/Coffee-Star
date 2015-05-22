3f8abc7d50d0c73af0ab9349d825e03b8a90fbef
Jonathan Eidelman Manevich <jonyxmaster@gmail.com>
2015-05-21 20:15:38 -0500

    Almost working perfectly, has some bugs though, and still missing the holiday calculations

--- p3/calendario.rb
+++ p3/calendario.rb
@@ -12,6 +12,31 @@ def dow(y, m, d)
   return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7
 end
 
+def printCalendar(y)
+ row = []
+  for i in 1..12
+    month = [[0, 1, 2, 3, 4, 5, 6]]
+    for j in 1..31
+      var = y[((i - 1) * 31) + j]
+      #puts var 
+      row.push(var)
+      #p row
+      if  y[((i - 1) * 31) + j] == 6
+        month.push(row)
+        row = []
+      end 
+    end
+    puts "Mes #{i}"
+    p month[0]
+    puts
+    for i in 1..month.count - 1
+      p month[i]
+    end 
+    puts
+  end
+
+end 
+
 def calendario(y)
   year = []
   year << nil
@@ -55,7 +80,9 @@ puts "Ingrese año el año a calcular"
 
 y = gets.chomp.to_i
 a = calendario y
+
+printCalendar a
+puts a.length
+puts a[35]
 p a
-
-
 
