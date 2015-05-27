79700601b479a31ee67c9c9350badbbaf1ac23a8
Jonathan Eidelman Manevich <jonyxmaster@gmail.com>
2015-05-26 22:43:35 -0500

    Added non-movable holidays of the year, all working just fine :)

--- p3/calendario.rb
+++ p3/calendario.rb
@@ -12,6 +12,42 @@ def dow(y, m, d)
   return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7
 end
 
+def calcularFestivos(y)
+  #1 de enero - Año Nuevo
+  #Jueves Santo
+  #Viernes Santo
+  #1 de mayo – Día del Trabajo
+  #20 de julio – Independencia Nacional 7 de agosto – Batalla de Boyacá
+  #8 de diciembre - Inmaculada Concepción 25 de diciembre - Navidad
+
+  # 1 de enero
+  y[1] *= -1
+
+  #1 de mayo
+
+  y[4*31 + 1] *= -1
+
+  #20 de julio
+
+  y[6*31 + 20] *= -1
+
+  #8 de diciembre
+
+  y[11*31 + 8] *= -1
+
+  #7 de agosto
+
+  y[7*31 + 7] *= -1
+
+  #25 de diciembre
+
+  y[11*31 + 25] *= -1
+
+
+  return y
+
+end
+
 def printCalendar(y)
   meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
   dia = 0
@@ -25,79 +61,137 @@ def printCalendar(y)
     puts
     j = 1
     while j <= 31
+
       if y[(i - 1)*31 + j] != nil
-        if y[(i - 1)*31 + j] == dia
-          row << j
+
+        if y[(i - 1)*31 + j].abs == dia
+
+          if y[(i - 1)*31 + j] <= 0
+
+            row << ("F " + j.to_s)
+
+          else
+
+            row << j if y[(i - 1)*31 + j] > 0
+
+          end
+
           dia = (dia + 1) % 7
           j += 1
-       else
+
+        else
+
           row << ""
           dia = (dia + 1) % 7
-        end 
-        if row.count == 7 || row[row.count - 1] == 31
+
+        end
+
+        if row.count == 7 || row[row.count - 1] == 31 || row[row.count - 1] == "F 31"
+
           p row
           row = []
           dia = 0
+
         end
-      else 
+
+      else
+
         p row if row.count > 0
         dia = 0
         row = []
         j = 32
-      end 
+
+      end
+
     end
+
     row = []
     puts
     puts
-  end 
-end 
+
+  end
+
+end
 
 def calendario(y)
   year = []
   year << nil
   day = dow(y,1,1)
-  #year << firstDay 
+  #year << firstDay
   for i in 1..12
+
     for j in 1..31
-      if i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12 
+
+      if i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12
+
         year << day
         day = (day + 1) % 7
+
       else
+
         if i == 2
+
           if j <= 28
-            year << day 
+
+            year << day
             day = (day + 1) % 7
-          end 
+
+          end
+
           if (y % 4 == 0 && y % 100 != 0) || (y % 4 == 0 && y % 100 == 0 && y % 400 == 0)
+
             if j == 29
-              year << day 
+
+              year << day
               day = (day + 1) % 7
-            end 
+
+            end
+
             if j  == 30 || j == 31
-              year << nil 
-            end
-          else
-            if j == 29 || j == 30 || j == 31
+
               year << nil
+
             end
-          end 
+
+          else
+
+            if j == 29 || j == 30 || j == 31
+
+              year << nil
+
+            end
+
+          end
+
         end
-      end 
+
+      end
+
       if (i == 4 || i == 6 || i == 11 || i == 9)
-        year << day if j <= 30 
+
+        year << day if j <= 30
         day = (day + 1) % 7 if j <= 30
         year << nil if j > 30
-      end 
-    end 
+
+      end
+
+    end
+
   end
+
   return year
-end 
+
+end
 
 #Main
 
-puts "Ingrese año el año a calcular"
+puts "Ingrese el año a calcular"
 
 y = gets.chomp.to_i
 a = calendario y
+a = calcularFestivos a
 printCalendar a
 
+puts "El 31 de Mayo es #{a[4*31 + 31]}"
+puts
+puts a.count
